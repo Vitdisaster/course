fedb2b8d00ad7825253a0f3f760155ff95bfc98a
Шишов Виталий Юрьевич <vshishov@phoenixit.ru>
2020-03-23 16:06:16 +0300

    Ищу элемент, подставляю значение икс в формулу и записываю ответ в текстовое поле

--- get_attribute.py
+++ get_attribute.py
@@ -8,4 +8,15 @@ try:
     driver = webdriver.Chrome()
     driver.get(link)
 
-	
+    pict = driver.find_element_by_id("treasure")
+    x = pict.get_attribute("valuex")
+    #x = x.text
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    print(x)
+    z = calc(x)
+    print("Найдено1")
+    answer = driver.find_element_by_id("answer")
+    answer.send_keys(z)
+    print("Вставлено")
+    
