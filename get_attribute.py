a5eaf8436c536536ea51c440b45027a923dad7d0
Шишов Виталий Юрьевич <vshishov@phoenixit.ru>
2020-03-23 16:03:58 +0300

    Отрываю ссылку на окно задания

---
+++ get_attribute.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time 
+import math
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    driver = webdriver.Chrome()
+    driver.get(link)
+
+	
